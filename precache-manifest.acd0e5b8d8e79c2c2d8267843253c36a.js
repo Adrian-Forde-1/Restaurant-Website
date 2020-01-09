caee54095857569b463516c0612dfa9ebcb75458
Adrian Forde <adrianforde@Adrians-MacBook-Pro.local>
2020-01-08 23:00:06 -0400

    Updates

---
+++ precache-manifest.acd0e5b8d8e79c2c2d8267843253c36a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b272242b902a91aca3dff89a965d293",
+    "url": "/Restaurant-Website/index.html"
+  },
+  {
+    "revision": "6d12d60f350369c9ddd5",
+    "url": "/Restaurant-Website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "61600c0f15a3eb200e2c",
+    "url": "/Restaurant-Website/static/js/2.794d898b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Restaurant-Website/static/js/2.794d898b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d12d60f350369c9ddd5",
+    "url": "/Restaurant-Website/static/js/main.828825d6.chunk.js"
+  },
+  {
+    "revision": "1e845a15b56dca6fcd09",
+    "url": "/Restaurant-Website/static/js/runtime-main.11425872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Restaurant-Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
