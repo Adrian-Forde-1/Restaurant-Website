c7237d2c9b38bfffb01477d619d0f92ecddcc8c1
Adrian Forde <adrianforde@Adrians-MacBook-Pro.local>
2020-01-22 15:55:55 -0400

    Added <Switch> To app.js to test if the website will load propperly onto the homepage using github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
@@ -9,16 +9,18 @@ import ReservationNavbar from './components/ReservationNavbar';
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
-        {/* <Navbar /> */}
+      <Switch>
+        <div className='App'>
+          {/* <Navbar /> */}
 
-        <Route exact path='/' component={Navbar} />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/reservation' component={ReservationNavbar} />
-        <Route path='/reservation' component={Reservation} />
+          <Route exact path='/' component={Navbar} />
+          <Route exact path='/' component={Home} />
+          <Route exact='/reservation' component={ReservationNavbar} />
+          <Route path='/reservation' component={Reservation} />
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
+      </Switch>
     </BrowserRouter>
   );
 }
